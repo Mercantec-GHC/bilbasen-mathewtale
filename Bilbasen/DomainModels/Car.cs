e9bd2352b36be37209cc0c1abf65d488f449926a
Budget knight <mads.bargsteen@live.dk>
2024-10-29 13:26:25 +0100

    abstract class og interface

--- Bilbasen/DomainModels/Car.cs
+++ Bilbasen/DomainModels/Car.cs
@@ -1,21 +1,41 @@
 ﻿namespace DomainModels
 {
-    public class Car
+
+    public abstract class Vehicle : IVehicle
     {
-        public string? Brand { get; set; }
-        public string? Model { get; set; }
+
         public int Year { get; set; }
         public string? Color { get; set; }
         public int HoursePower { get; set; }
         public int NumberOfCylinders { get; set; }
+        public string? Fueltype { get; set; }
 
-        public Car(string? brand, string? model, int year, string? color, int hoursePower)
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+
+        public string? Price { get; set; }
+
+    }
+
+    public interface IVehicle
+    {
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public string? Price { get; set; }
+    }
+
+    class Car : Vehicle
+    { 
+    public Car(string? brand, string? model, int year, string? color, int hoursePower)
         {
             Brand = brand;
             Model = model;
             Year = year;
             Color = color;
             HoursePower = hoursePower;
-        }
-    }
+            NumberOfCylinders = NumberOfCylinders;
+       
+        }}
+
+
 }
